89fa874418e520698195cb42b57960e191652ec1
drashtivaghela02 <drashtivaghela02@gmail.com>
2024-02-27 18:27:38 +0530

    Added Meals Model data

--- Screens/MealDetailsScreen.js
+++ Screens/MealDetailsScreen.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../coponents/HeaderButton';
+
+import { AntDesign } from '@expo/vector-icons';
 
 const MealDetailsScreen = props => {
+
+    const mealId = props.navigation.getParam('mealId');
+    const selectedMeals = MEALS.find(meal => meal.id === mealId)
+
     return (
-        <View style = {styles.screen} >
-            <Text>The Meal Details Screen!</Text>
-        </View>
+        <View style={styles.screen}>
+            <Text>{selectedMeals.title}</Text>
+            <Button
+                title="Go Back to Categories"
+                onPress={() => {
+                    props.navigation.popToTop();
+                }}
+            />
+      </View>
     );
+};
+
+MealDetailsScreen.navigationOptions = navigationData => {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeals = MEALS.find(meal => meal.id === mealId);
+
+    return{
+        headerTitle: selectedMeals.title,
+        headerRight: (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <AntDesign name="staro" size={24} onPress={()=> {
+                console.log("Mark as Favorite!!");
+            }} />
+        </HeaderButtons>
+        )
+    };
 };
 
 const styles = StyleSheet.create({
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
 import CategoriesScreens from '../Screens/CategoriesScreen';
 import CategoryMealsScreen from '../Screens/CategoryMealsScreen';
 import MealDetailsScreen from '../Screens/MealDetailsScreen';
+import FavoritesScreens from '../Screens/FavoritesScreens';
 import Colors from '../constatnts/Colors';
+
+import { Ionicons } from '@expo/vector-icons';
 
 const MealsNavigator = createStackNavigator({
     Categories: {
@@ -26,4 +31,31 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcons : (tabInfo) => {
+                return (
+                    <Ionicons name="restaurant" size={25} color={tabInfo.tintColor} />
+                );
+            }
+        }
+    },
+    Favorites: {
+        screen: FavoritesScreens,
+        navigationOptions: {
+            tabBarIcons : (tabInfo) => {
+                return (
+                    <AntDesign name="staro" size={25} color={tabInfo.tintColor} />
+                );
+            }
+        }
+    }
+},{
+    tabBarOptions:{
+        activeTintColor: Colors.accentColor
+    }
+});
+
+export default createAppContainer(MealsFavTabNavigator);
